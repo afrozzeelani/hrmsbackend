d8e6a7339bc7d2266c6b36f92fd315e9b748458c
Zeelani <65568348+afrozzeelani@users.noreply.github.com>
2024-06-11 07:40:36 +0000

    routes

--- routes/departmentRoute.js
+++ routes/departmentRoute.js
@@ -3,22 +3,22 @@
 const express = require('express');
 const departmentRoute = express.Router();
 
-// const cityController = require('../controllers/cityController');
+const cityController = require('../controllers/cityController');
 const {verifyHR, verifyAdminHR} = require('../middleware/authMiddleware');
 
 const { getAllDepartment, createDepartment, updateDepartment, deleteDepartment } = require('../controllers/DepartmentController');
 
 // GET: Retrieve all countries
 // verifyHR
-departmentRoute.get("/department", verifyAdminHR,  getAllDepartment);
+departmentRoute.get("/department",   getAllDepartment);
 
 // POST: Create a new city
-departmentRoute.post("/department", verifyAdminHR,  createDepartment);
+departmentRoute.post("/department",  createDepartment);
 
 // PUT: Update an existing city
-departmentRoute.put("/department/:id", verifyAdminHR,  updateDepartment);
+departmentRoute.put("/department/:id",  updateDepartment);
 
 // DELETE: Delete a city
-departmentRoute.delete("/department/:id", verifyAdminHR,  deleteDepartment);
+departmentRoute.delete("/department/:id",  deleteDepartment);
 
 module.exports = departmentRoute;
